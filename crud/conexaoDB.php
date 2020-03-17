9ef805ab123517ff8f2039624eb3b68241180c86
Erick Rodrigues <erickgamerh2o@gmail.com>
2020-03-16 22:05:42 -0300

    Foi Criado a Função de cadastro para o cliente e Ajuste da String de conexao (conexaoBD.php)

--- crud/conexaoDB.php
+++ crud/conexaoDB.php
@@ -1,22 +1,14 @@
+
+
+
 <?php
 
 
 
-$conexao = mysqli_connect("localhost", "root", "");
-$bancoSelect = mysqli_select_db($conexao, "barbershopbd");
-
-if($conexao){
-	echo"Conexão realizada com sucesso";
-
-}
-else{
-	die("Falha na conexão : ->".mysqli_error());
-}
-
-
-
-
+$conexao = mysqli_connect("localhost", "root","", "barbershopbd", "3308");
 
 
 
 ?>
+
+
