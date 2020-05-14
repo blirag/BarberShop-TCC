8cab9902e1d73c5632c2d955d94ffc828173f72c
Beatriz Lira <beatrizliragonzaga@gmail.com>
2020-05-14 14:15:06 -0300

    Modificando adicionar lucro

--- crud/adicionarlucro.php
+++ crud/adicionarlucro.php
@@ -4,21 +4,27 @@ require_once 'conexaoDB.php';
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
-    $sql = mysqli_query($conexao, "SELECT valor FROM tb_agendamento WHERE idAgendamento = '$id'");
+    $sql = mysqli_query($conexao, "SELECT valor, dataAgendamento FROM tb_agendamento WHERE idAgendamento = '$id'");
     $dados = mysqli_fetch_array($sql);
+    $data = $dados['dataAgendamento'];
+    $mes_agendamento = idate('m', strtotime($data));
+    date_default_timezone_set('America/Sao_Paulo');
+    $mes_atual = idate('m');
     $valor = $dados['valor'];
+    
+    if($mes_agendamento == $mes_atual){
+        $insert = "INSERT INTO tb_lucros (valor) VALUES ('$valor')";
 
-    $insert = "INSERT INTO tb_lucros (valor) VALUES ('$valor')";
-
-    if(mysqli_query($conexao, $insert)){
-        if($_SESSION['funcionario']){
-            header ('location: ../perfis/perfilfuncionario');
+        if(mysqli_query($conexao, $insert)){
+            if($_SESSION['funcionario']){
+                header ('location: ../perfis/perfilfuncionario');
+            }
+            else if($_SESSION['proprietario']){
+                header ('location: ../perfis/perfilproprietario');
+            }
         }
-        else if($_SESSION['proprietario']){
-            header ('location: ../perfis/perfilproprietario');
-        }
-    }
-    else{
+        else{
         echo "<script language='javascript' type='text/javascript'> alert('Erro, não foi possível realizar está ação!');window.location = '../perfis/perfilfuncionario.php'</script>";
+        }
     }
 }
